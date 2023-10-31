c3d6d0ee981a21e1e49ef903d5247fe689cb731e
최주연 <genboyjy@gmail.com>
2023-10-31 09:30:31 +0900

    initial commit

---
+++ src/main/java/com/mzbr/videoencodingservice/VideoEncodingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mzbr.videoencodingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoEncodingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoEncodingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mzbr/videoencodingservice/VideoEncodingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzbr.videoencodingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoEncodingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
