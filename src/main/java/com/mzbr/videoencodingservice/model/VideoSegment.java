cf0139259ee29a83d442c7e23f06939dc158ddbe
최주연 <genboyjy@gmail.com>
2023-10-31 13:40:15 +0900

    :zap: feat: 엔티티 추가

---
+++ src/main/java/com/mzbr/videoencodingservice/model/VideoSegment.java
@@ -0,0 +1,26 @@
+package com.mzbr.videoencodingservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "videoSegment")
+public class VideoSegment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String videoUrl;
+}
