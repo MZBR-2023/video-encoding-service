530ddb593dc42cebd90b4c963ff9ba475702f0b4
최주연 <genboyjy@gmail.com>
2023-11-15 09:04:49 +0900

    feat: encodeFormat에 status 추가로 쓸모없는 메소드 제거

--- src/main/java/com/mzbr/videoencodingservice/service/DynamoService.java
+++ src/main/java/com/mzbr/videoencodingservice/service/DynamoService.java
@@ -79,7 +79,7 @@ public class DynamoService {
 	}
 
 	public void updateEncodingStatusToTrue(Long id, EncodeFormat format, int index) throws Exception {
-		String attributeName = getStatusAttributeName(format);
+		String attributeName = format.getStatusName();
 
 		String updateExpression = String.format("SET %s[%d] = :status", attributeName, index);
 
@@ -117,20 +117,5 @@ public class DynamoService {
 
 		m3U8Service.updateMasterM3u8(id, encodeFormats);
 
-	}
-
-	private String getStatusAttributeName(EncodeFormat format) {
-		switch (format) {
-			case P144:
-				return "p144Status";
-			case P360:
-				return "p360Status";
-			case P480:
-				return "p480Status";
-			case P720:
-				return "p720Status";
-			default:
-				throw new IllegalArgumentException("Unknown format: " + format);
-		}
 	}
 }
